a32ee513c62e05d0f7db3f6fe83ebb0c7dcb6bdd
aroura <32834623+arouraus@users.noreply.github.com>
2023-10-22 00:07:32 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -10,5 +10,21 @@ serve(async (request) => {
   }
 
   url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
+
+  // 修改 headers 来指定使用 HTTP/1.1 版本
+  const modifiedRequest = new Request(request.url, {
+    ...request,
+    headers: {
+      ...request.headers,
+      "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Dest": "document",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    },
+  });
+
+  return await fetch(url, modifiedRequest);
 });
