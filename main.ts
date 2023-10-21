70758fd298f36cefd0647468a6f7fa85918e31aa
aroura <32834623+arouraus@users.noreply.github.com>
2023-10-21 23:57:56 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,18 +1,5 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
-const OPENAI_API_HOST = "api.openai.com";
-
-serve(async (request) => {
-  const url = new URL(request.url);
-
-  if (url.pathname === "/") {
-    return new Response(await Deno.readTextFile("./Readme.md"), {
-      headers: {
-        "content-type": "text/plain;charset=UTF-8",
-      },
-    });
-  }
-
-  url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
+serve(() => {
+  return fetch(new URL("./Readme.md", import.meta.url));
 });
